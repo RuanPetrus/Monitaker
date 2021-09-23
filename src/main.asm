fd3fa3f149504bcd9e3490efb1ad1a7cfffed83d
Gabriel Felix <gabfelixdelima@gmail.com>
2021-09-23 19:24:26 -0300

    Melhorar compatibilidade de video.asm com main.asm
    Este commit transforma video.asm em uma "biblioteca" de labels para melhorar
    a modularidade do código. Para esta mudança, é necessário mover seu .include
    para o final do arquivo, evitando assim execuções acidentais.
    
    Algumas refatorações menores também foram feitas. Todos os "jr ra" foram
    substituídos por "ret" para melhorar a legibilidade e os returns recursivos
    agora retornam o valor armazenado de seu retorno para o registrador "ra" de
    modo que agora é possível voltar com "ret".

--- src/main.asm
+++ src/main.asm
@@ -1,7 +1,14 @@
-.include "video.asm"
-
 .text
 INIT:	
+	# Video test
+	jal INIT_VIDEO
+	li a0,0x07070707 # red
+	jal PAINT_SCREEN
+	jal SWAP_FRAMES
+	li a0,0x38383838 # green
+	jal PAINT_SCREEN
+	jal SWAP_FRAMES
+
 	la t0, D_BG_MUSIC
 	la t1, M_BG	
 	sw t1, 0(t0)	
@@ -11,5 +18,6 @@ G_LOOP:	jal a6,KEY1
 	jal P_MUS
 	j G_LOOP
 
+.include "video.asm"
 .include "sound.asm"
 .include "poling-non-blocking.asm"
